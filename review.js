74f322f8b45b9c70655bd7f3cbd3365f94af18ee
Editingboi998 <csang998@gmail.com>
2020-12-29 15:40:05 +1300

    v1.0.1

---
+++ review.js
@@ -0,0 +1,36 @@
+// variable declarations
+let x; // let is for variables that can change
+const a; // const is for variables that have a constant value
+var y; // var is for variable that can change (old version of let)
+// just use let or const
+
+// naming conventions
+// variables should be named consistently, the most popular one is camel case
+// variables in javascript can't have spaces so in variables that have multible words,
+// the first letter of the words after the first word is capatilized
+let x = appleSauce;
+
+// types of variables
+let x = 2; // this is a Number
+let boolean = true; // this is a boolean value (true/false)
+let txt = "tree"; // this is a String, can be defined using: "" '' ``
+let array = [0,2,4,8]; // arrays are number indexed lists of variables
+array[1] // is 2, the [] square brackets (aka sub)
+// objects are like arrays but have properties, aka key/value pairs
+let object = {
+  a: 'apple',
+  b: 'bear'
+};
+// functions can also be defined as variables:
+// using the function keyword
+function doAction0() {
+
+}
+// using the arrow function syntax
+let doAction1 = () => {
+
+};
+
+// function can return
+
+// funcitons with paramters
